0a642fc32b150b02ce0ea065a39f06c7e29a17d1
kewinexpertep <--global>
2019-07-02 15:51:50 +0700

    http server

--- index.py
+++ index.py
@@ -4,6 +4,8 @@ import time
 import RPi.GPIO as GPIO
 import os
 import dropbox
+import SimpleHTTPServer
+import SocketServer
 # import schedule
 import Adafruit_DHT
 sensor = Adafruit_DHT.DHT22
@@ -137,6 +139,23 @@ while connection:
         time.sleep(10)
         connection = True
         logging.warning("disconnect")
+
+PORT = 8000
+class ServerHandler(SimpleHTTPServer.SimpleHTTPRequestHandler):
+
+    def do_POST(self):
+        content_len = int(self.headers.getheader('content-length', 0))
+        post_body = self.rfile.read(content_len)
+        print post_body
+        microgear.publish("/pH",post_body.split(",")[0],{'retain':True})
+        microgear.publish("/EC",post_body.split(",")[1],{'retain':True})
+Handler = ServerHandler
+
+httpd = SocketServer.TCPServer(("", PORT), Handler)
+
+print "serving at port", PORT
+httpd.serve_forever()
+
 while True:
     humidityIn, temperatureIn = Adafruit_DHT.read_retry(sensor, pinDHTin)
     humidityOut, temperatureOut = Adafruit_DHT.read_retry(sensor, pinDHTout)
